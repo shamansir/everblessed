261e69d3ee147b010a26630a27f2ea0acabb8cb7
kenan238 <kenanyazbeck@gmail.com>
2023-01-22 18:36:33 +0200

    added streambox

---
+++ test/widget-streambox.js
@@ -0,0 +1,31 @@
+var blessed = require('../src/lib/reblessed')
+  , screen;
+const { read } = require('fs');
+const Stream = require("stream");
+
+screen = blessed.screen({
+  dump: __dirname + '/logs/textarea.log',
+  fullUnicode: true,
+  warnings: true
+});
+
+var box = blessed.streambox({
+  parent: screen,
+  style: {
+    bg: 'blue'
+  },
+  height: 'half',
+  width: 'half',
+  top: 'center',
+  left: 'center',
+  tags: true,
+  stream: new Stream.Readable(),
+});
+
+box.stream.push('Hello streams!');
+
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+  return process.exit(0);
+});
+
+screen.render();
